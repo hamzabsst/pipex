ef6366776752e9e74bdac887310caedc533460f1
Hamza Boussetta <hbousset@c3r1p5.1337.ma>
2025-01-26 10:26:30 +0100

    makefile

---
+++ Makefile
@@ -0,0 +1,35 @@
+CC = cc
+CFLAGS = -Wall -Werror -Wextra
+
+MYLIB_DIR = mylib
+MYLIB = $(MYLIB_DIR)/myLib.a
+
+NAME = pipex
+
+SRCS =	src/main.c \
+		handle_errors.c \
+
+OBJS = $(SRCS:.c=.o)
+
+all: $(MYLIB) $(NAME)
+
+$(MYLIB):
+		@$(MAKE) -C $(MYLIB_DIR)
+
+%.o: %.c inc/pipex.h
+		@$(CC) $(CFLAGS) -c $< -o $@
+
+$(NAME): $(OBJS) $(MYLIB)
+		@$(CC) $(CFLAGS) $(OBJS) $(MYLIB) -o $(NAME)
+
+clean:
+		@rm -f $(OBJS)
+		@$(MAKE) clean -C $(MYLIB_DIR)
+
+fclean: clean
+		@rm -f $(NAME)
+		@$(MAKE) fclean -C $(MYLIB_DIR)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- src/main.c
+++ src/main.c
@@ -6,7 +6,15 @@
 /*   By: hbousset <hbousset@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 09:32:30 by hbousset          #+#    #+#             */
-/*   Updated: 2025/01/25 09:32:31 by hbousset         ###   ########.fr       */
+/*   Updated: 2025/01/26 10:09:33 by hbousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "../inc/pipex.h"
+
+int	main(int ac, char **av)
+{
+	if (check_errors(ac, av))
+		return (1);
+
+}
